cfe1423f92b5a825c3a39e271a14a4c159010c09
Nick Momrik <nick@momrik.com>
2016-12-15 00:41:43 -0500

    Make it pretty.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var sensor = require('node-dht-sensor');
 var Lcd = require('lcd');
+var moment = require('moment');
 
-lcd = new Lcd({rs: 13, e: 19, data: [16, 20, 21, 26], cols: 16, rows: 2});
+lcd = new Lcd({
+	rs: 13,
+	e: 19,
+	data: [16, 20, 21, 26],
+	cols: 16,
+	rows: 2
+});
 
 function printRow(string, row, cb) {
 	lcd.setCursor(0, row);
@@ -16,23 +23,22 @@ function printRow(string, row, cb) {
 	});
 }
 
-process.on('SIGINT', function () {
-	lcd.close();
-	process.exit();
-});
+function doSensorRead() {
+	sensor.read(22, 17, function(err, temperature, humidity) {
+		if (!err) {
+			var date = moment().format(' MMM D   hh:mm');
+			var info = ' ' + (temperature * 1.8 + 32).toFixed(1) + String.fromCharCode(223) + 'F   ' + humidity.toFixed(1) + '%';
+
+			console.log(date, info);
+			printRow(date, 0, function() {
+				printRow(info, 1);
+			});
+		}
+	});
+}
 
 lcd.on('ready', function() {
-	setInterval(function () {
-		sensor.read(22, 17, function(err, temperature, humidity) {
-			if (!err) {
-				var temp = (temperature * 1.8 + 32).toFixed(1);
-				var hum  = humidity.toFixed(1);
-				var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+	doSensorRead();
 
-				printRow(date, 0, function() {
-					printRow(temp + 'F - ' + hum + '%', 1);
-				});
-			}
-		});
-	}, 10000 );
+	setInterval(doSensorRead, 10000);
 });
